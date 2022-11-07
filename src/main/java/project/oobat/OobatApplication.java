718d753597d0f73cf3990f98a0220aaded0cf321
Dzakirin Asyraff <dzakirinasyraff@gmail.com>
2022-11-07 21:22:40 +0800

    Project Init

---
+++ src/main/java/project/oobat/OobatApplication.java
@@ -0,0 +1,13 @@
+package project.oobat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OobatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OobatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/oobat/OobatApplicationTests.java
@@ -0,0 +1,13 @@
+package project.oobat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OobatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
