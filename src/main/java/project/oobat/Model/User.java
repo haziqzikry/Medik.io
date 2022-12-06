33c2d6afc9c950edcb809a2cf1a9b85ed745d2de
Dzakirin Asyraff <dzakirinasyraff@gmail.com>
2022-12-06 22:18:07 +0800

    Added User Model

---
+++ src/main/java/project/oobat/Model/User.java
@@ -0,0 +1,33 @@
+package project.oobat.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Setter@Getter
+public class User {
+    
+    @Id
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+}
