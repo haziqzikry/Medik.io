ddd9541e36cdeae4eb0f16770c087f9641e57b69
Dzakirin Asyraff <dzakirinasyraff@gmail.com>
2023-01-07 16:02:36 +0800

    Update User Index

--- src/main/java/project/oobat/Controller/UserController.java
+++ src/main/java/project/oobat/Controller/UserController.java
@@ -13,6 +13,24 @@ public class UserController {
         return "userindex";
     }
     
+    @GetMapping("/profile")
+    public String profile() {
+        return "userprofile";
+    }
+
+    @GetMapping("/view-order")
+    public String viewOrder() {
+        return "vieworder";
+    }
+
+    @GetMapping("/view-product")
+    public String viewProduct() {
+        return "product";
+    }
     
+    @GetMapping("/view-cart")
+    public String viewCart() {
+        return "cart";
+    }
 
 }
