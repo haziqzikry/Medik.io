3e92e86ac0f6b509fd3d0b3a218beb935230503c
Muhammad Aniq Aqil Bin Azrai Fahmi <aniqaqil06@gmail.com>
2023-01-27 01:50:31 +0800

    manage profile

--- src/main/java/project/oobat/Controller/UserController.java
+++ src/main/java/project/oobat/Controller/UserController.java
@@ -1,11 +1,16 @@
 package project.oobat.Controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import project.oobat.Model.AppUser;
+import project.oobat.Service.AppUserService;
 import project.oobat.Service.ProductService;
 
 @Controller
@@ -13,15 +18,24 @@ import project.oobat.Service.ProductService;
 public class UserController {
 
     @Autowired
-    private ProductService productService;
-    
+    private AppUserService appUserService;
+
     @GetMapping("/home")
     public String home() {
         return "user/home";
     }
-    
+
     @GetMapping("/profile")
-    public String profile() {
+    public String profile(Model model, Principal principal) {
+        model.addAttribute("user", appUserService.loadUserByUsername(principal.getName()));
         return "user/profile";
     }
+
+    @PostMapping("/profile")
+    public String profile(AppUser appUser) {
+        appUserService.updateProfile(appUser);
+
+        return "redirect:user/profile";
+    }
+
 }
--- src/main/java/project/oobat/Service/AppUserService.java
+++ src/main/java/project/oobat/Service/AppUserService.java
@@ -18,7 +18,7 @@ import project.oobat.Model.AppUser.Role;
 import project.oobat.Repository.AppUserRepository;
 
 @Service
-public class AppUserService implements UserDetailsService{
+public class AppUserService implements UserDetailsService {
 
     @Autowired
     private AppUserRepository appUserRepository;
@@ -29,33 +29,37 @@ public class AppUserService implements UserDetailsService{
     @Override
     public AppUser loadUserByUsername(String username) throws UsernameNotFoundException {
         return appUserRepository.findByUsername(username).orElseThrow(
-            () -> new UsernameNotFoundException("User not found"));
+                () -> new UsernameNotFoundException("User not found"));
     }
 
-    public void registerCustomer(AppUser appUser){
+    public void registerCustomer(AppUser appUser) {
         appUser.setPassword(bCryptPasswordEncoder.encode(appUser.getPassword()));
         // line below could be problematic
         appUser.setRole(Role.CUSTOMER);
         appUserRepository.save(appUser);
     }
 
-    public void registerPharmacist(AppUser appUser){
+    public void registerPharmacist(AppUser appUser) {
         appUser.setPassword(bCryptPasswordEncoder.encode(appUser.getPassword()));
         // line below could be problematic
         appUser.setRole(Role.PHARMACIST);
         appUserRepository.save(appUser);
     }
 
-    public List<Object> isUserExist(String username){
+    public List<Object> isUserExist(String username) {
         boolean isExist = false;
         String message = "";
         Optional<AppUser> result = appUserRepository.findByUsername(username);
-        if(result.isPresent()){
+        if (result.isPresent()) {
             isExist = true;
             message = "Username already exist";
         }
         System.out.println("isExist: " + isExist);
         System.out.println("message: " + message);
         return Arrays.asList(isExist, message);
+    }
+
+    public void updateProfile(AppUser appUser) {
+        appUserRepository.save(appUser);
     }
 }
